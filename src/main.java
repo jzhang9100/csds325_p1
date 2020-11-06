3bc4f007c769b6dd39c10a1ae21876ea5a09fe25
cxz41 <jackzhang9100@yahoo.com>
2020-11-05 23:20:56 -0500

    config file

--- src/main.java
+++ src/main.java
@@ -1,10 +1,19 @@
 import network.HTTPServer;
 import network.HTTPClient;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class main {
     public static void main(String args[]) throws Exception {
         System.out.println("Running...");
         System.out.println("Sever initilized...");
-        (new Thread(new HTTPServer())).start();
+
+        Properties prop = new Properties();
+        FileInputStream ip = new FileInputStream("/home/cxz416/csds325_p1/src/static/config.properties");
+
+        prop.load(ip);
+        int port = (int) prop.get("port");
+        (new Thread(new HTTPServer(port))).start();
     }
 }
